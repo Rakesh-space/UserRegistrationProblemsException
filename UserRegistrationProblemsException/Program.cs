110f928da81b034a0680a93d9fb7a46de5d837f7
Rakesh-space <rakeshsmusale03@gmail.com>
2021-11-12 12:38:03 +0530

    Welcome message to UserRegistration by Exception handling.

---
+++ UserRegistrationProblemsException/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationProblemsException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to User Registration by using Exception handling!");
+        }
+    }
+}
